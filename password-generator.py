257d2ba1ce1fd50c7b8528fa6879924514647d4e
Derek muriuki <98864170+dee-mee@users.noreply.github.com>
2024-02-13 10:48:56 +0300

    password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -3,5 +3,5 @@ import random
 
 pieces = string.ascii_letters + string.values + string.punctuation
 length = 10
-password = "".join(random.string(pieces, length))
+password = "".join(random.sample(pieces, length))
 print(password)
